def723d95403566601bfc039e3a865d1ca700fef
David R. MacIver <david@drmaciver.com>
2017-03-14 07:53:24 +0000

    Factor out regex code into its own dedicated module

--- test_falbs.py
+++ test_falbs.py
@@ -1,5 +1,6 @@
 import hypothesis.strategies as st
-import falbs as rd
+import regex as rd
+from falbs import Simulator, compute_generating_functions
 from hypothesis import given, assume
 
 
@@ -34,13 +35,13 @@ def test_can_build_a_dfa(re):
 
 @given(regex())
 def test_can_compute_a_generating_function(re):
-    rd.compute_generating_functions(*rd.build_dfa(re))
+    compute_generating_functions(*rd.build_dfa(re))
 
 
 @given(regex(), st.integers())
 def test_can_simulate_accurately(regex, seed):
     assume(rd.has_matches(regex))
-    sim = rd.Simulator(regex, seed)
+    sim = Simulator(regex, seed)
     d = sim.draw(0.001)
     for _ in range(10):
         assert rd.matches(regex, next(d))
