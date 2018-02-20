30de79199644d091b336f90fb41979285d3c1e0f
David R. MacIver <david@drmaciver.com>
2018-02-20 09:05:55 +0000

    Update Hypothesis config for new health checks

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
-from hypothesis import settings
+from hypothesis import settings, HealthCheck
+
+settings.register_profile(
+    'default',
+    settings(
+        deadline=None,
+        suppress_health_check=[HealthCheck.too_slow],
+    ))
+
+settings.load_profile('default')
 
 settings.register_profile(
     'coverage', settings(max_examples=0),
